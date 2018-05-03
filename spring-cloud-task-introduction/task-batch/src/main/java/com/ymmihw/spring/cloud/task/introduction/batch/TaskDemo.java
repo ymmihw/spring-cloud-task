0048fb10fc6dc9832bdc28eef578ad32a4ca8a03
wanghaoming <whimmy@126.com>
2018-05-03 14:20:51 +0800

    clean up

--- spring-cloud-task-introduction/task-batch/src/main/java/com/ymmihw/spring/cloud/task/introduction/batch/TaskDemo.java
+++ spring-cloud-task-introduction/task-batch/src/main/java/com/ymmihw/spring/cloud/task/introduction/batch/TaskDemo.java
@@ -1,15 +1,12 @@
 package com.ymmihw.spring.cloud.task.introduction.batch;
 
 import java.util.logging.Logger;
-import javax.sql.DataSource;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -18,19 +15,6 @@ import org.springframework.stereotype.Component;
 public class TaskDemo {
 
   private final static Logger LOGGER = Logger.getLogger(TaskDemo.class.getName());
-
-  @Autowired
-  private DataSource dataSource;
-
-  @Bean
-  public HelloWorldTaskConfigurer getTaskConfigurer() {
-    return new HelloWorldTaskConfigurer(dataSource);
-  }
-
-  @Bean
-  public TaskListener taskListener() {
-    return new TaskListener();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(TaskDemo.class, args);
--- spring-cloud-task-introduction/task-batch/src/main/java/com/ymmihw/spring/cloud/task/introduction/batch/TaskListener.java
+++ spring-cloud-task-introduction/task-batch/src/main/java/com/ymmihw/spring/cloud/task/introduction/batch/TaskListener.java
@@ -3,7 +3,9 @@ package com.ymmihw.spring.cloud.task.introduction.batch;
 import java.util.logging.Logger;
 import org.springframework.cloud.task.listener.TaskExecutionListener;
 import org.springframework.cloud.task.repository.TaskExecution;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TaskListener implements TaskExecutionListener {
 
   private final static Logger LOGGER = Logger.getLogger(TaskListener.class.getName());
