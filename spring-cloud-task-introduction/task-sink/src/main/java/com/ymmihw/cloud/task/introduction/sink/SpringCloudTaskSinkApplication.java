a313e95e05f7746a258f0e107d2382dd57e2841c
wanghaoming <whimmy@126.com>
2018-05-03 10:58:38 +0800

    add task-sink

---
+++ spring-cloud-task-introduction/task-sink/src/main/java/com/ymmihw/cloud/task/introduction/sink/SpringCloudTaskSinkApplication.java
@@ -0,0 +1,13 @@
+package com.ymmihw.cloud.task.introduction.sink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.launcher.annotation.EnableTaskLauncher;
+
+@SpringBootApplication
+@EnableTaskLauncher
+public class SpringCloudTaskSinkApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudTaskSinkApplication.class, args);
+  }
+}
---
+++ spring-cloud-task-introduction/task-sink/src/main/java/com/ymmihw/cloud/task/introduction/sink/TaskSinkConfiguration.java
@@ -0,0 +1,15 @@
+package com.ymmihw.cloud.task.introduction.sink;
+
+import static org.mockito.Mockito.mock;
+import org.springframework.cloud.deployer.spi.task.TaskLauncher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class TaskSinkConfiguration {
+  @Bean
+  public TaskLauncher taskLauncher() {
+    return mock(TaskLauncher.class);
+  }
+}
